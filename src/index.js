f66e126ded99bf757606c24d8916fff279495738
Your Name <you@example.com>
2023-03-24 09:28:02 -0400

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import MovieList from './MovieList';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MovieList />
   </React.StrictMode>
 );
 
